85e8ea1aa0c9c6ad026de0acd653b4dc95cf824c
maksymdiakiv <dyakiv.max@gmail.com>
2023-05-12 14:57:28 +0200

    lab

---
+++ lab9/script.js
@@ -0,0 +1,23 @@
+// let car = {
+//     name : "audi",
+//     model : "a6",
+//     color : "green",
+//     year : 2017,
+//     speed : 250
+// }
+// car.increaseSpeed = function(){
+//     car.speed += 200
+// }
+// car.decreaseSpeed = function(){
+//     car.speed -= 200
+// }
+// car.information = function(){
+//     console.log(car.name + " " + car.model + " " + "(" + car.year + ")" + "," + " " + "Kolor : " + car.color + "," + " prędkośc : " + car.speed)
+// }
+// console.log(car.speed)
+// car.increaseSpeed()
+// console.log(car.speed)
+// car.decreaseSpeed()
+// car.decreaseSpeed()
+// console.log(car.speed)
+// car.information()
